7355eb42d17c48c78963eb5768007adec636bbdd
Reldalk <Reldalk@gmail.com>
2018-12-14 15:30:09 -0500

    squaredancing complete

--- queue-drills.js
+++ queue-drills.js
@@ -16,7 +16,34 @@ function display(queue){
   return output;
 }
 
+function squareDancing(maleQueue, femaleQueue){
+  while(maleQueue.first && femaleQueue.first){
+    console.log(maleQueue.dequeue() + ' with ' + femaleQueue.dequeue());
+  }
+  console.log('forever alone');
+  while(maleQueue.first){
+    console.log('M ' + maleQueue.dequeue());
+  }
+  while(femaleQueue.first){
+    console.log(femaleQueue.dequeue());
+  }
+}
+
+function enqueueDancers(maleQueue, femaleQueue){
+  const array = ['F Jane', 'M Frank', 'M John', 'M Sherlock', 'F Madonna', 'M David', 'M Christopher', 'F Beyonce']
+  array.forEach(name => {
+    if(name.charAt(0) === 'F'){
+      femaleQueue.enqueue(name.slice(2));
+    }
+    else{
+      maleQueue.enqueue(name.slice(2));
+    }
+  });
+}
+
 function main(){
+
+  /*
   const starTrekQ = new Queue();
   starTrekQ.enqueue('Kirk');
   starTrekQ.enqueue('Spock');
@@ -32,6 +59,10 @@ function main(){
   starTrekQ.dequeue();
 
   console.log(display(starTrekQ));
-
+*/
+  let maleQueue = new Queue();
+  let femaleQueue = new Queue();
+  enqueueDancers(maleQueue, femaleQueue);
+  squareDancing(maleQueue, femaleQueue);
 }
 main();
